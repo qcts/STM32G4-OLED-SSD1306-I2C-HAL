a086a44654a0690a0a2220596a9e36e0df309911
qlqqs <qcts@qlqqs.com>
2024-11-17 20:28:33 +0800

    Update OLED_Data.h

--- Core/Hardware/OLED_Data.h
+++ Core/Hardware/OLED_Data.h
@@ -3,14 +3,24 @@
 
 #include <stdint.h>
 
-/*中文字符字节宽度*/
-#define OLED_CHN_CHAR_WIDTH         3       //UTF-8编码格式给3，GB2312编码格式给2
+/*字符集定义*/
+/*以下两个宏定义只可解除其中一个的注释*/
+#define OLED_CHARSET_UTF8           //定义字符集为UTF8
+//#define OLED_CHARSET_GB2312       //定义字符集为GB2312
 
 /*字模基本单元*/
 typedef struct
 {
-    char Index[OLED_CHN_CHAR_WIDTH + 1];    //汉字索引
-    uint8_t Data[32];                       //字模数据
+
+#ifdef OLED_CHARSET_UTF8            //定义字符集为UTF8
+    char Index[5];                  //汉字索引，空间为5字节
+#endif
+
+#ifdef OLED_CHARSET_GB2312          //定义字符集为GB2312
+    char Index[3];                  //汉字索引，空间为3字节
+#endif
+
+    uint8_t Data[32];               //字模数据
 } ChineseCell_t;
 
 /*ASCII字模数据声明*/
