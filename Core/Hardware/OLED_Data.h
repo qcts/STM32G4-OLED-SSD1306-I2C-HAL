f31e38619b30ba58ae4d6666aa23395bb8dae117
qlqqs <qcts@qlqqs.com>
2024-11-17 22:27:35 +0800

    Update OLED_Data.h

--- Core/Hardware/OLED_Data.h
+++ Core/Hardware/OLED_Data.h
@@ -5,22 +5,21 @@
 
 /*字符集定义*/
 /*以下两个宏定义只可解除其中一个的注释*/
-#define OLED_CHARSET_UTF8           //定义字符集为UTF8
-//#define OLED_CHARSET_GB2312       //定义字符集为GB2312
+#define OLED_CHARSET_UTF8                /* 定义字符集为UTF8 */
+//#define OLED_CHARSET_GB2312            /* 定义字符集为GB2312 */
 
 /*字模基本单元*/
 typedef struct
 {
-
-#ifdef OLED_CHARSET_UTF8            //定义字符集为UTF8
-    char Index[5];                  //汉字索引，空间为5字节
+#ifdef OLED_CHARSET_UTF8
+    char Index[5];                      /* 汉字索引，空间为5字节 */
 #endif
 
-#ifdef OLED_CHARSET_GB2312          //定义字符集为GB2312
-    char Index[3];                  //汉字索引，空间为3字节
+#ifdef OLED_CHARSET_GB2312
+    char Index[3];                      /* 汉字索引，空间为3字节 */
 #endif
 
-    uint8_t Data[32];               //字模数据
+    uint8_t Data[32];                   /* 字模数据 */
 } ChineseCell_t;
 
 /*ASCII字模数据声明*/
